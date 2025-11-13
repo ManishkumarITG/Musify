c7ea0e40577dd0a02f5e5ff286470ee22179e21c
ManishKumarITG <manishkumar@itgeeks.com>
2025-11-13 14:02:36 +0530

    api

---
+++ src/services/API.js
@@ -0,0 +1,12 @@
+const BASE_URL = "https://love-lyrics-backend.vercel.app/api/v1/"
+
+
+export async function getAllTrack(){
+    try {
+        let res = await fetch(`${BASE_URL}Album/GetAllAlbum`)
+        let data = await res.json();
+        return data;
+    } catch (error) {
+        console.log("get all track api ", error);
+    }
+}
